c3898f4d7fd18460ec94546cfbbfa2a16a0077f0
JupiteR <nut065r@gmail.com>
2025-04-17 21:08:41 +0700

    แก้ไข function ที่ matplotlib deprecate

--- infer/lib/train/utils.py
+++ infer/lib/train/utils.py
@@ -215,16 +215,42 @@ def latest_checkpoint_path(dir_path, regex="G_*.pth"):
     return x
 
 
+# def plot_spectrogram_to_numpy(spectrogram):
+#     global MATPLOTLIB_FLAG
+#     if not MATPLOTLIB_FLAG:
+#         import matplotlib
+
+#         matplotlib.use("Agg")
+#         MATPLOTLIB_FLAG = True
+#         mpl_logger = logging.getLogger("matplotlib")
+#         mpl_logger.setLevel(logging.WARNING)
+#     import matplotlib.pylab as plt
+#     import numpy as np
+
+#     fig, ax = plt.subplots(figsize=(10, 2))
+#     im = ax.imshow(spectrogram, aspect="auto", origin="lower", interpolation="none")
+#     plt.colorbar(im, ax=ax)
+#     plt.xlabel("Frames")
+#     plt.ylabel("Channels")
+#     plt.tight_layout()
+
+#     fig.canvas.draw()
+#     data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep="")
+#     data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+#     plt.close()
+#     return data
+
 def plot_spectrogram_to_numpy(spectrogram):
     global MATPLOTLIB_FLAG
     if not MATPLOTLIB_FLAG:
         import matplotlib
-
         matplotlib.use("Agg")
         MATPLOTLIB_FLAG = True
+        import logging
         mpl_logger = logging.getLogger("matplotlib")
         mpl_logger.setLevel(logging.WARNING)
-    import matplotlib.pylab as plt
+
+    import matplotlib.pyplot as plt
     import numpy as np
 
     fig, ax = plt.subplots(figsize=(10, 2))
@@ -234,11 +260,17 @@ def plot_spectrogram_to_numpy(spectrogram):
     plt.ylabel("Channels")
     plt.tight_layout()
 
+    # Draw and get the image buffer
     fig.canvas.draw()
-    data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep="")
-    data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+    buf = fig.canvas.buffer_rgba()
+    data = np.frombuffer(buf, dtype=np.uint8).copy()  # .copy() to make it writable
+    data = data.reshape(fig.canvas.get_width_height()[::-1] + (4,))
     plt.close()
+
+    # Optionally drop alpha channel if you want RGB only
+    data = data[:, :, :3]
     return data
+
 
 
 def plot_alignment_to_numpy(alignment, info=None):
